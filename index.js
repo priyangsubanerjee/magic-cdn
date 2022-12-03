71e690ff57459abd289fb5b51ee294a97f83fe2d
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-12-03 17:54:16 +0530

    const

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-// const multer = require("multer");
-// const dotenv = require("dotenv");
-// const { uploadFile, getFileStream } = require("./backend/s3");
-// const fs = require("fs");
-// const util = require("util");
-// const unlinkFile = util.promisify(fs.unlink);
-// const cors = require("cors");
-// const upload = multer({
-//   dest: "tmp/",
-// });
+const multer = require("multer");
+const dotenv = require("dotenv");
+const { uploadFile, getFileStream } = require("./backend/s3");
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
+const cors = require("cors");
+const upload = multer({
+  dest: "tmp/",
+});
 
-// dotenv.config();
-// app.use(cors());
-// app.use(bodyParser.json());
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname, "frontend")));
 
 // app.post("/", upload.single("data"), async function (req, res) {
